9e8aca4a86057e424aa75175ddc991917580bb1c
SurajPaudel <soorajpaudel@gmail.com>
2025-09-08 11:07:16 -0400

    eventlisterner

---
+++ script.js
@@ -0,0 +1,14 @@
+//selecting elements to manipulate
+const addtaskbtn=document.getElementById("addtaskbtn");
+const taskinput=document.getElementById("taskinput");
+
+// adding event lister to button
+addtaskbtn.addEventListener("click",()=>{
+    const task= taskinput.value
+    if(task=== ""){
+        alert("please enter a task");
+        return;
+    }
+    console.log("Task added:", task);
+    taskinput.value=""; //clears input after adding 
+})
